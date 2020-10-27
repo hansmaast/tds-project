c711d4bff48a2132ae8c8799963ab3ed9a2b1cc1
Hans Maast <hfmaast@gmail.com>
2020-10-27 08:49:12 +0100

    change config to be platform spesific

--- src/utils/nhost.js
+++ src/utils/nhost.js
@@ -1,9 +1,26 @@
 import nhost from 'nhost-js-sdk';
+import { isPlatform } from '@ionic/react';
+import { Plugins } from '@capacitor/core';
 import { BACKEND_URL } from '../constants/secrets';
 
-const config = {
-  base_url: BACKEND_URL,
-};
+const { Storage } = Plugins;
+
+let config;
+
+if (isPlatform('capacitor')) {
+  config = {
+    base_url: BACKEND_URL,
+    use_cookies: false,
+    client_storage: Storage,
+    client_storage_type: 'capacitor',
+  };
+} else {
+  config = {
+    base_url: BACKEND_URL,
+    use_cookies: true,
+    client_storage_type: 'web',
+  };
+}
 
 nhost.initializeApp(config);
 
