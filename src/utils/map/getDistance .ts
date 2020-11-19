baf150926025b0221ab7a6c39b006ef8118a7ddc
Hans Maast <hfmaast@gmail.com>
2020-11-19 23:32:35 +0100

    add measure func in map

---
+++ src/utils/map/getDistance .ts
@@ -0,0 +1,27 @@
+import { LngLat } from 'mapbox-gl';
+
+export const getDistanceInMeters = (
+  { from: a, to: b } : {from: LngLat, to: LngLat},
+): number => a.distanceTo(b);
+
+export const getTotalDistanceInMeters = (
+  { from: setOfCoordinates }:{from: LngLat[]},
+): number => {
+  let meters: number = 0;
+
+  const setDistance = () => {
+    const { length } = setOfCoordinates;
+    if (length === 0) return;
+    setOfCoordinates.forEach((coor, i) => {
+      if (i + 1 === length) return;
+      const a = setOfCoordinates[i];
+      const b = setOfCoordinates[i + 1];
+      const distance = getDistanceInMeters({ from: a, to: b });
+      meters += distance;
+    });
+  };
+  setDistance();
+
+  console.log('Total distance: ', meters);
+  return meters;
+};
